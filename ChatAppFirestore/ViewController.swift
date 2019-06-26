dc379ebd0a833c4458582b9c7a1d9390bf6b6470
OuSS <ouss.cs@gmail.com>
2019-06-26 18:41:28 +0100

    Initial Commit

---
+++ ChatAppFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatAppFirestore
+//
+//  Created by OuSS on 6/26/19.
+//  Copyright © 2019 OuSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
